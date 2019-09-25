e7b1836da64e1cfa91185cae1a0dcb5451572beb
xiaomi <2667987974@qq.com>
2019-09-25 22:03:28 +0800

    创建测试

---
+++ src/main/java/com/xm/bootdemo/Controller/Test.java
@@ -0,0 +1,18 @@
+package com.xm.bootdemo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: xiaomi
+ * @CreateDate: 2019/9/25$ 21:34$
+ * @Version: 1.0
+ */
+@RestController
+public class Test {
+    @GetMapping("/test")
+    public String test(){
+        return "hello world";
+    }
+}
